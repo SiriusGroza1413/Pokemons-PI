32120e9382c7913fe63eb00647ba92648174a84d
Guillermo Ojeda <gro1413@hotmail.com>
2023-02-14 22:08:12 -0300

    fix db deploy

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const port = process.env.PORT || 3001
+const port = process.env.PORT
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
